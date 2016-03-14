c378294f352c11b846b72d33b90cc45aaf7b9807
Matheus Faria de Alencar <mtsalenc@gmail.com>
2016-03-14 00:48:54 -0300

    Added copy to clipboard

--- Assets/ShowToast.cs
+++ Assets/ShowToast.cs
@@ -1,30 +1,51 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ShowToast : MonoBehaviour {
 
 	string toastString;
+	string input;
 	AndroidJavaObject currentActivity;
+	AndroidJavaClass UnityPlayer;
+	AndroidJavaObject context;
 
-	public void MyShowToastMethod ()
-	{		
+	void Start(){
+		if(Application.platform == RuntimePlatform.Android){
+			UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"); 
+			context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+		}
+	}
+		
+	public void ToAndroidClipBoard(InputField txtField){
 		if (Application.platform == RuntimePlatform.Android) {
-			showToastOnUiThread ("It Worked!");
+			input = txtField.text;
+			currentActivity.Call ("runOnUiThread", new AndroidJavaRunnable (toClipBoardOnUiThread));
 		}
 	}
 
-	void showToastOnUiThread(string toastString){
-		AndroidJavaClass UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+	void toClipBoardOnUiThread(){
+		Debug.Log (this+": Running on UI thread");
 
-		currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"); 
+		AndroidJavaClass Context = new AndroidJavaClass("android.content.Context");
+		AndroidJavaObject CLIPBOARD_SERVICE = Context.GetStatic<AndroidJavaObject> ("CLIPBOARD_SERVICE");
+		AndroidJavaObject clipboardMgr = currentActivity.Call<AndroidJavaObject> ("getSystemService",CLIPBOARD_SERVICE);
+		AndroidJavaClass ClipData = new AndroidJavaClass ("android.content.ClipData");
+		AndroidJavaObject clipData = ClipData.CallStatic<AndroidJavaObject> ("newPlainText","deltaBit",input);
+		clipboardMgr.Call ("setPrimaryClip", clipData);
+
+		showToastOnUiThread ("Copied to clipboard: " + input);
+	}
+
+	void showToastOnUiThread(string toastString){		
 		this.toastString = toastString;
-
 		currentActivity.Call ("runOnUiThread", new AndroidJavaRunnable (showToast));
 	}
 
-	void showToast(){
-		Debug.Log ("Running on UI thread");
-		AndroidJavaObject context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+	void showToast(){		
+		Debug.Log (this+": Running on UI thread");
+
 		AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
 		AndroidJavaObject javaString=new AndroidJavaObject("java.lang.String",toastString);
 		AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject> ("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_SHORT"));
