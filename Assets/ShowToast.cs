7d3fc13b2d49b0f10c3404f94603355da57dbf6e
Zakir Shikhli <zakirshikhli@gmail.com>
2018-10-14 11:46:55 +0400

    Minor changes

--- Assets/ShowToast.cs
+++ Assets/ShowToast.cs
@@ -19,10 +19,44 @@ public class ToastMessageScript : MonoBehaviour
     }
 
 
+       string toastString;
+    readonly string input;
+    AndroidJavaObject currentActivity;
+    AndroidJavaClass UnityPlayer;
+    AndroidJavaObject context;
+
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
+        }
+    }
+
+
     public void ShowToast(string toastString)
     {
-        this.toastString = toastString;
-        currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(Toast));
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            this.toastString = toastString;
+            currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(Toast));
+        }
+        else
+        {
+            Debug.Log("Toast: " + toastString, this);
+        }
+    }   
+
+    void Toast()
+    {
+        Debug.Log(this + ": Running on UI thread");
+
+        AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
+        AndroidJavaObject javaString = new AndroidJavaObject("java.lang.String", toastString);
+        AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject>("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_LONG"));
+        toast.Call("show");
     }
 
     void Toast()
