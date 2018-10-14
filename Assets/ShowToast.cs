d9cd0cee0568613699144da19af7b03c28d50c40
Zakir Shikhli <zakirshikhli@gmail.com>
2018-10-14 11:36:41 +0400

    Minor changed

--- Assets/ShowToast.cs
+++ Assets/ShowToast.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ToastMessageScript : MonoBehaviour
 {
     string toastString;
-    string input;
+    readonly string input;
     AndroidJavaObject currentActivity;
     AndroidJavaClass UnityPlayer;
     AndroidJavaObject context;
@@ -17,21 +17,21 @@ public class ToastMessageScript : MonoBehaviour
             context = currentActivity.Call<AndroidJavaObject>("getApplicationContext");
         }
     }
-    
 
-    public void showToastOnUiThread(string toastString)
+
+    public void ShowToast(string toastString)
     {
         this.toastString = toastString;
-        currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(showToast));
+        currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(Toast));
     }
 
-    void showToast()
+    void Toast()
     {
         Debug.Log(this + ": Running on UI thread");
 
         AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
         AndroidJavaObject javaString = new AndroidJavaObject("java.lang.String", toastString);
-        AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject>("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_SHORT"));
+        AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject>("makeText", context, javaString, Toast.GetStatic<int>("LENGTH_LONG"));
         toast.Call("show");
     }
 }
